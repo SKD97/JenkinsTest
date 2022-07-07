08797994edcecdfeb22026885ddc2ba352dd7fc2
SKD97 <kambojsimran941@gmail.com>
2022-07-07 18:57:06 +0530

    Adding all the changes

---
+++ src/main/java/com/email/sender/emailSender/TestClass.java
@@ -0,0 +1,10 @@
+package com.email.sender.emailSender;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, First Project");
+
+	}
+
+}
--- src/main/java/com/email/sender/emailSender/exception/AddressNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.email.sender.emailSender.exception;
-
-public class AddressNotFoundException {
-
-}
